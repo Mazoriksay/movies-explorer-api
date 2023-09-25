7008b7c63033e15689b9bdb5097e34d853dc01de
Nikita Kovalev <mazoriksay.aod@gmail.com>
2023-09-25 04:09:51 +0300

    added corsPolicy

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { errors } = require('celebrate');
 
 const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { corsPolicy } = require('./middlewares/corsPolicy');
 const { rateLimiter } = require('./utils/rateLimiter');
 
 const router = require('./routes/index');
@@ -16,6 +17,7 @@ const NotFoundError = require('./errors/NotFoundError');
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
+app.use(corsPolicy);
 app.use(helmet());
 app.use(cookieParser());
 app.use(rateLimiter);
