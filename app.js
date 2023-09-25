a5cf0d16668267fa2d18d5496188308313c1a814
Nikita Kovalev <mazoriksay.aod@gmail.com>
2023-09-25 05:19:07 +0300

    fix cors

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process
 
 const app = express();
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
