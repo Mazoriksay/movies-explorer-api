011181595bebfb7d4496d334cae505fb8bfe944a
Nikita Kovalev <mazoriksay.aod@gmail.com>
2023-09-25 04:48:20 +0300

    fix cors

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
+
 
 const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { corsPolicy } = require('./middlewares/corsPolicy');
+
 const { rateLimiter } = require('./utils/rateLimiter');
 
 const router = require('./routes/index');
@@ -17,7 +19,9 @@ const NotFoundError = require('./errors/NotFoundError');
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
-app.use(corsPolicy);
+app.use(cors({
+  origin: '*',
+}));
 app.use(helmet());
 app.use(cookieParser());
 app.use(rateLimiter);
